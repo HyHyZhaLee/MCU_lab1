c86f3139924d07b733e88a32a34734c267e286a4
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2023-09-24 23:51:15 +0700

    Update ex10

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -120,14 +120,43 @@ int main(void)
 	  else if(num == 10)HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, ON_STATE);
 	  else if(num == 11)HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, ON_STATE);
   }
+  void clearNumberOnClock(int num){
+	  if(num == 0) HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, OFF_STATE);
+	  else if(num == 1)HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, OFF_STATE);
+	  else if(num == 2)HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, OFF_STATE);
+	  else if(num == 3)HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, OFF_STATE);
+	  else if(num == 4)HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, OFF_STATE);
+	  else if(num == 5)HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, OFF_STATE);
+	  else if(num == 6)HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, OFF_STATE);
+	  else if(num == 7)HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, OFF_STATE);
+	  else if(num == 8)HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, OFF_STATE);
+	  else if(num == 9)HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, OFF_STATE);
+	  else if(num == 10)HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, OFF_STATE);
+	  else if(num == 11)HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, OFF_STATE);
+  }
 #include "software_timer.h"
-  setTimer(1000);
+  setTimer(5000);
+  int sec = 0, min = 0, hour = 0;
+  clearAllClock();
+  setNumberOnClock(0);
   while (1)
   {
 	  if(timer_flag){
-		  setTimer(1000);
+		  setTimer(5000);
+		  clearNumberOnClock(sec++);
+		  if(sec>=12) {
+			  sec = 0;
+			  clearNumberOnClock(min++);
+			  if(min>=12) {
+				  min = 0;
+				  clearNumberOnClock(hour++);
+				  if(hour>=12) hour = 0;
+			  }
+		  }
+		  setNumberOnClock(sec);
+		  setNumberOnClock(min);
+		  setNumberOnClock(hour);
 	  }
-
 	  timerRun();
 	  HAL_Delay(10);
 
